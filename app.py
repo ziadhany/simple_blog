2de95c3f5caf8512e1af45d0c5da7a43b77d68cb
ziadhany <ziadhany2016@gmail.com>
2023-05-19 23:23:45 +0300

    Initial conf blog app

---
+++ app.py
@@ -0,0 +1,70 @@
+from flask import Flask, render_template, redirect, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    password = db.Column(db.String(30), nullable=False)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    title = db.Column(db.String(100))
+    body = db.Column(db.Text)
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return f'<Post {self.title}>'
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/')
+def index():
+    posts = Post.query.all()
+    return render_template('index.html', posts=posts)
+
+
+@app.route('/logout')
+def logout():
+    return 'Logout'
+
+
+@app.route('/login')
+def login():
+    return 'Login'
+
+
+@app.route('/create', methods=['POST'])
+def create():
+    title = request.form['title']
+    body = request.form['body']
+
+    new_post = Post(title=title, body=body)
+    db.session.add(new_post)
+    db.session.commit()
+
+    return redirect('/')
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('error_404.html'), 404
+
+
+@app.errorhandler(500)
+def not_found(error):
+    return render_template('error_500.html'), 500
+
+
+if __name__ == '__main__':
+    app.run()
